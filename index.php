43956cc118d0f295cc0c218ec6cf5ab2279e06fa
gilmae <avocadia@fastmail.fm>
2018-01-24 22:14:49 +1100

    Route based on a route table
    Route table looks too much like regexes, the regex boiler plate should be added by the code, so we can remove the delimiters
    
    Also, did too much, so it is already compiling a hash of tokens => values found in the route, should have been left to a later step.

--- index.php
+++ index.php
@@ -13,20 +13,48 @@
 
 $path_only = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-switch ($path_only){
-    case "/":
-        echo("Whatsoever thy hand findeth to do, do it with thy might; for there is no work, nor device, nor knowledge, nor wisdom, in the grave, whither thou goest");
-        break;
-    case "/quote/id":
-        echo("It's a single quote");
-        break;
-    case "/quote/new":
-        echo("Its a form");
-        break;
-    case "/quote/create":
-        echo("Saving yer quote");
-        break;
+
+
+
+function find_route($path)
+{
+    $routes = array(
+        '/^\/$/' => "Whatsoever thy hand findeth to do, do it with thy might; for there is no work, nor device, nor knowledge, nor wisdom, in the grave, whither thou goest",
+        '/^\/quote\/new$/' => "It's a quote form",
+        '/^\/quote\/create$/' => 'Its a script to save the quote',
+        '/^\/quote\/:id$/' => "It's a single quote"
+    );
+
+    foreach ($routes as $key => $value)
+    {
+        $matches;
+        $keyAsRegex = translate_route_key_to_regex($key);
+        if (preg_match($keyAsRegex, $path, $matches) == 1) {
+            $tokens = scan_route_key_for_tokens($key);
+            $token_values = array_slice($matches, 1);
+            $route_args = array_combine($tokens, $token_values);
+            
+            return $value;
+        }
+    }
+
+    return "Nope";
 }
+
+function translate_route_key_to_regex($key)
+{
+    return preg_replace("/(:[^\/$]+)/", "(.+)", $key);
+}
+
+function scan_route_key_for_tokens($key)
+{
+    $matches;
+    if (preg_match("/:([^\/$]+)/", $key, $matches) == 1) {
+        return array_slice($matches, 1);
+    }
+
+}
+echo(find_route($path_only));
  
 
 
