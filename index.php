8d4cdfb1d70ef62132d3dd7ba8639f3c2282e2c6
gilmae <avocadia@fastmail.fm>
2018-01-24 21:18:39 +1100

    Defined the pages required with a proposed url structure

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<?php
+ /* 
+ * A Quotes collection site
+ * 
+ * What pages do we need to for this?
+ * / => A page to display a list, in chronological order
+ * /quote/:id => A detail page to display more information about the quote
+ * /quote/new => A form to submit the quote
+ * /quote/create => The script that performs the save
+ * 
+ */
+
+
+
+?>
 Whatsoever thy hand findeth to do, do it with thy might; for there is no work, nor device, nor knowledge, nor wisdom, in the grave, whither thou goest
