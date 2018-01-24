2fc90005b015cabc7af1a7c524f9fec285d1cf06
gilmae <avocadia@fastmail.fm>
2018-01-24 21:36:55 +1100

    Some hardcode, noddy routing

--- index.php
+++ index.php
@@ -11,6 +11,24 @@
  */
 
 
+$path_only = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($path_only){
+    case "/":
+        echo("Whatsoever thy hand findeth to do, do it with thy might; for there is no work, nor device, nor knowledge, nor wisdom, in the grave, whither thou goest");
+        break;
+    case "/quote/id":
+        echo("It's a single quote");
+        break;
+    case "/quote/new":
+        echo("Its a form");
+        break;
+    case "/quote/create":
+        echo("Saving yer quote");
+        break;
+}
+ 
+
 
 ?>
-Whatsoever thy hand findeth to do, do it with thy might; for there is no work, nor device, nor knowledge, nor wisdom, in the grave, whither thou goest
+
