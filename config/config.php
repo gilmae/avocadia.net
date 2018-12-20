fe1bead8c904f885a08cec0e9340a0994098c921
gilmae <avocadia@fastmail.fm>
2018-12-20 16:34:24 +1100

    Remove config file from git

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    DEFINE("DB_HOST", "127.0.0.1");
-    DEFINE("DB_NAME", "quotes");
-    DEFINE("DB_CHARSET", "utf8mb4");
-    DEFINE("DB_UID", "quotes");
-    DEFINE("DB_PWD", "");
-    
-?>
