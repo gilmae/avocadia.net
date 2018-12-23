93179fbfc71d02dd752257d75ba02caf6d2b694c
gilmae <avocadia@fastmail.fm>
2018-12-24 10:37:53 +1100

    Clean out copy pasted code

--- config/queries.php
+++ config/queries.php
@@ -1,5 +1,2 @@
 <?php
-    DB::registerQuery('find_all_highlights', "select * from highlights order by createdAt desc");
-    DB::registerQuery('find_highlight', 'select * from highlights where id = :id');
-    DB::registerQuery('add_highlight', "insert into highlights (quote, source, author) values (:quote, :source, :author);");
 ?>
